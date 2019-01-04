df99ce973daacf3ff1f7f9f4a23bf1babcaa1b63
Vincent Fong <vfong00@stuy.edu>
2019-01-04 11:25:09 -0500

    initialized some files

---
+++ Nethack.java
@@ -0,0 +1,5 @@
+public class Nethack {
+  public static void main(String[] args) {
+    
+  }
+}
---
+++ Player.java
@@ -0,0 +1,6 @@
+public class Player extends Entity {
+  public Player(int x, int y, char sprite) {
+    super(0,0,'@');
+  }
+  
+}
