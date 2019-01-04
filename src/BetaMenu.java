0ce70cc90e7c97b109b37e14224e9ac691b74e25
Moody <mrahman01@stuy.edu>
2019-01-04 17:27:32 -0500

    optimized main to utilize new printMenu via PrintString

--- src/BetaMenu.java
+++ src/BetaMenu.java
@@ -8,17 +8,22 @@ import java.io.IOException;
 
 public class BetaMenu{
 
-public static void main(String[] args) throws IOException {
-	Terminal t = new UnixTerminal();
-	t.enterPrivateMode();
-	t.setCursorVisible(false);
-	int menpos = 15;
+public static void printMenu(int menpos, Terminal t){
 	Color red = new Color(ANSI.RED, ANSI.DEFAULT);
 	Print.printString(20, 15, "start", red, t);
 	Print.printString(20, 16, "help", red, t);
 	Print.printString(19, menpos, ">", red, t);
 	Print.printString(30, 30, "select with space, w and s to select options, q to quit", red, t);
 	Print.printString(30, 21, "DO NOT HIT THE ARROW KEYS OR ELSE U DIE", red, t);
+}
+
+public static void main(String[] args) throws IOException {
+	int menpos = 15;
+	Terminal t = new UnixTerminal();
+	t.enterPrivateMode();
+	t.setCursorVisible(false);
+
+	BetaMenu.printMenu(menpos, t);
 
 
 	try {
