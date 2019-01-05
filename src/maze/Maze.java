c234f90be29affa4a6ce4266b60fabaa90fd5a3b
Moody <mrahman01@stuy.edu>
2019-01-05 10:01:21 -0500

    this is the Maze Object

---
+++ src/maze/Maze.java
@@ -0,0 +1,30 @@
+import com.googlecode.lanterna.terminal.*;
+import com.googlecode.lanterna.terminal.ansi.*;
+
+public class Maze{
+
+Object[][] maze;
+
+public Maze(){
+	maze = new Object[40][100];
+}
+
+public Object getMaze(){
+	return maze;
+}
+
+public void setTile(int x, int y, Object toSet){
+	maze[x][y] = toSet;
+}
+
+public void printMaze(Terminal t) throws IOException{
+	for (int y = 29; y >= 0; y--) {
+                for (int x = 0; x < 100; x++) {
+                        t.setCursorPosition(x, y);
+                }
+		System.out.println();
+        }
+}
+
+
+}
