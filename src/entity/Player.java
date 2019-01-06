920bcb9e2dcf0357499e6936768f417c6a273167
Moody <mrahman01@stuy.edu>
2019-01-05 19:54:26 -0500

    player entity collision logic, all implemented within player!

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,6 +1,43 @@
 public class Player extends Entity {
-  public Player(int x, int y, Maze map) {
-    super(x,y,'@', map);
-  }
+
+
+public Player(int x, int y, Maze map) {
+	super(x,y,'@', map);
+}
+
+private boolean toMove(char dir){
+	if(dir == 'w'){
+		if ( getMap().getMaze()[getX()][getY() - 1].getType().equals("entity") ) {
+			return false;
+		}
+		moveTo(getX(), getY() - 1, getMap());
+		return true;
+	}
+
+	if(dir == 'a'){
+		if (getMap().getMaze()[getX() - 1][getY()].getType() .equals("entity")){
+			return false;
+		}
+		moveTo(getX() - 1, getY(), getMap());
+		return true;
+	}
+
+	if(dir == 's'){
+		if (getMap().getMaze()[getX()][getY() + 1].getType() .equals("entity")){
+			return false;
+		}
+		moveTo(getX(), getY() + 1, getMap());
+		return true;
+	}
+
+	if(dir == 'd'){
+		if (getMap().getMaze()[getX() + 1][getY()].getType() .equals("entity")){
+			return false;
+		}
+		moveTo(getX() + 1, getY(), getMap());
+		return true;
+	}
+	return false;
+}
 
 }
