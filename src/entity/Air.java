a459e5a9883d90bba9eb1ac9ddd4f9431835ba97
Moody <mrahman01@stuy.edu>
2019-01-05 19:08:09 -0500

    air! as "thing that can exist when nothing else exist"

---
+++ src/entity/Air.java
@@ -0,0 +1,24 @@
+public class Air implements Tileable{
+
+private int x, y;
+private char sprite = ' ';
+
+public Air(int x, int y){}
+
+public int getX(){
+	return x;
+}
+
+public int getY(){
+	return y;
+}
+
+public char getSprite(){
+	return sprite;
+}
+
+public String getType(){
+	return "air";
+}
+
+}
