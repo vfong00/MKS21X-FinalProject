9e8921013fb8ed9cd0776c714d02c00d7421bb0e
Moody <mrahman01@stuy.edu>
2019-01-05 19:13:10 -0500

    revamped netHack to use new features (no key interactions)

--- src/game/Nethack.java
+++ src/game/Nethack.java
@@ -15,58 +15,30 @@ public static void main(String[] args) throws IOException {
         ExtendedTerminal terminal = new UnixTerminal();
         terminal.enterPrivateMode();
         terminal.setCursorVisible(false);
-        TextGraphics textGraphics = terminal.newTextGraphics();
+        // TextGraphics textGraphics = terminal.newTextGraphics();
 
         // wall generation??
         for (int i = 15; i < 26; i++) {
         	// textGraphics.setCharacter(12, i, '#');
-        	maze.setTile(12, i, new Wall(12, i));
+        	new Wall(12, i, maze);
         }
 
         // player movement
-        Player p = new Player();
+        Player p = new Player(10, 10, maze);
 
-        int x = p.getX();
-        int y = p.getY();
         boolean running = true;
 
 	while (running){
-		terminal.setCursorPosition(x,y);
-		terminal.setBackgroundColor(ANSI.YELLOW);
-		terminal.setForegroundColor(ANSI.RED);
-		terminal.putCharacter(p.getSprite());
-		terminal.setBackgroundColor(ANSI.DEFAULT);
-		terminal.setForegroundColor(ANSI.DEFAULT);
+		// maze.printMaze(terminal);
+		terminal.setCursorPosition(0, 0);
+		maze.printMaze(terminal);
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e){}
+
 		KeyStroke key = terminal.readInput();
-
-		if (key.getCharacter().equals('q')) {
-			terminal.exitPrivateMode();
-			System.exit(0);
-		}
-
-		if ((key.getCharacter().equals('a')) && (textGraphics.getCharacter(x - 1, y) == null)) {
-			terminal.setCursorPosition(x,y);
-			terminal.putCharacter(' ');
-			x--;
-		}
-
-		if ((key.getCharacter().equals('d')) && (textGraphics.getCharacter(x + 1, y) == null)) {
-			terminal.setCursorPosition(x,y);
-			terminal.putCharacter(' ');
-			x++;
-		}
-
-		if ((key.getCharacter().equals('w')) && (textGraphics.getCharacter(x, y - 1) == null)) {
-			terminal.setCursorPosition(x,y);
-			terminal.putCharacter(' ');
-			y--;
-		}
-
-		if ((key.getCharacter().equals('s')) && (textGraphics.getCharacter(x, y + 1) == null)) {
-			terminal.setCursorPosition(x,y);
-			terminal.putCharacter(' ');
-			y++;
-		}
+		terminal.exitPrivateMode();
+		System.exit(0);
 	}
 }
 }
