5d7c54d396afcffdec95b63251610fbd750210f8
Moody <mrahman01@stuy.edu>
2019-01-08 11:47:45 -0500

    removed comments

--- src/game/Nethack.java
+++ src/game/Nethack.java
@@ -75,18 +75,6 @@ public static void main(String[] args) throws IOException {
 		if (c == 'p'){
 			terminal.clearScreen();
 			maze.calcGenerated(g, maze);
-			// g.generate();
-			// gen = g.getGenerated();
-			// for(int x = 0; x < 100; x++){
-			// 	for(int y = 0; y < 30; y++){
-			// 		if (gen[x][y] == '0'){
-			// 			maze.setTile(x, y, new Wall(x, y, maze));
-			// 		}
-			// 		else{
-			// 			maze.setTile(x, y, new Air(x, y));
-			// 		}
-			// 	}
-			// }
 		}
 		p.moveViaInput(c);
 		s.refresh(Screen.RefreshType.DELTA);
