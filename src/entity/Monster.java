ffa3aff6be8d94ba0bc0d08a1c5d98eb39a37dc9
Moududur "Moody" Rahman <mrahman01@stuy.edu>
2019-01-10 18:58:52 -0500

    remade monster

--- src/entity/Monster.java
+++ src/entity/Monster.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Monster extends Entity{
 
 private int hp;
@@ -6,5 +8,19 @@ private int damage;
 public Monster(int x, int y, int hp, int damage, Maze map){
 	super(x, y, hp, 5, 'Q', map);
 }
+public void nextMove() {
+	Random rng = new Random();
+	int option = rng.nextInt(4);
 
+	if ((option == 0) && (!getMap().getMaze()[getX()][getY() + 1].getType().equals("entity"))) {
+		moveTo(getX(),getY() + 1, getMap());
+	} else if ((option == 1) && (!getMap().getMaze()[getX()][getY() - 1].getType().equals("entity"))) {
+		moveTo(getX(),getY() - 1, getMap());
+	} else if ((option == 2) && (!getMap().getMaze()[getX() - 1][getY()].getType().equals("entity"))) {
+		moveTo(getX() - 1,getY(), getMap());
+	} else if (!getMap().getMaze()[getX() + 1][getY()].getType().equals("entity") ){
+		moveTo(getX() + 1,getY(), getMap());
+	}
+
+}
 }
