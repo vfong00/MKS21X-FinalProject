d4aa069968068bfb069607cdd22bfae29d08d4a2
Moududur "Moody" Rahman <mrahman01@stuy.edu>
2019-01-10 18:17:06 -0500

    d

--- src/entity/Monster.java
+++ src/entity/Monster.java
@@ -1,35 +1,14 @@
-import java.util.Random;
-import java.util.ArrayList;
+public class Monster extends Entity{
 
-public class Monster extends Entity {
+private int hp;
+private int damage;
 
-
-public Monster(int x, int y, int hp, int damage, char sprite, Maze map) {
-	super(x,y,hp, damage, sprite, map);
+public Monster(int x, int y, int hp, int damage, Maze map){
+	super(x, y, hp, 5, 'Q', map);
 }
 
-public void nextMove() {
-	Random rng = new Random();
-	int option = rng.nextInt(4);
+public void mover(){
 
-	if ((option == 0) && (!getMap().getMaze()[getX()][getY() + 1].getType().equals("entity"))) {
-		moveTo(getX(),getY() + 1, getMap());
-	} else if ((option == 1) && (!getMap().getMaze()[getX()][getY() - 1].getType().equals("entity"))) {
-		moveTo(getX(),getY() - 1, getMap());
-	} else if ((option == 2) && (!getMap().getMaze()[getX() - 1][getY()].getType().equals("entity"))) {
-		moveTo(getX() - 1,getY(), getMap());
-	} else if (!getMap().getMaze()[getX() + 1][getY()].getType().equals("entity") ){
-		moveTo(getX() + 1,getY(), getMap());
-	}
-
-	if (this.getHP() <= 0) {
-		this.setDead(true);
-		this.die();
-	}
-}
-
-public String getType(){
-	return "monster";
 }
 
 }
