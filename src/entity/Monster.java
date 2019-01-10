e2b52c94a0c2c631ed6943ea2ab866c99b1c2687
Vincent Fong <vfong00@stuy.edu>
2019-01-10 18:52:40 -0500

    made monsters work again

--- src/entity/Monster.java
+++ src/entity/Monster.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Monster extends Entity{
 
 private int hp;
@@ -5,6 +7,29 @@ private int damage;
 
 public Monster(int x, int y, int hp, int damage, Maze map){
 	super(x, y, hp, 5, 'Q', map);
+}
+
+public void nextMove() {
+	Random rng = new Random();
+	int option = rng.nextInt(4);
+	boolean dead = false;
+
+	if (!dead) {
+		if ((option == 0) && (!getMap().getMaze()[getX()][getY() + 1].getType().equals("entity"))) {
+			moveTo(getX(),getY() + 1, getMap());
+		} else if ((option == 1) && (!getMap().getMaze()[getX()][getY() - 1].getType().equals("entity"))) {
+			moveTo(getX(),getY() - 1, getMap());
+		} else if ((option == 2) && (!getMap().getMaze()[getX() - 1][getY()].getType().equals("entity"))) {
+			moveTo(getX() - 1,getY(), getMap());
+		} else if (!getMap().getMaze()[getX() + 1][getY()].getType().equals("entity") ){
+			moveTo(getX() + 1,getY(), getMap());
+		}
+
+		if (this.getHP() <= 0) {
+			dead = true;
+			this.die();
+		}
+	}
 }
 
 }
