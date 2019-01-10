ca275b257951453948edbe2a11af8daedac5439f
Moody Rahman <moududur921@gmail.com>
2019-01-10 13:29:34 -0500

    mosters can move

--- src/entity/Monster.java
+++ src/entity/Monster.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Monster extends Entity{
 
 private int hp;
@@ -5,6 +7,11 @@ private int damage;
 
 public Monster(int x, int y, int hp, int damage, Maze map){
 	super(x, y, hp, 5, 'Q', map);
+}
+
+public void move(Random r){
+	int movx = r.nextInt();
+	int movy = r.nextInt();
 }
 
 }
