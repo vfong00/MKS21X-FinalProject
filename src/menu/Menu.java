8c1fa20d663b6866b47c7d29084b412fb7e6861b
Moududur "Moody" Rahman <mrahman01@stuy.edu>
2019-01-14 08:32:54 -0500

    menu now uses vincents putString (remake it with color objects later)

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -11,9 +11,19 @@ import java.util.Random;
 
 public class Menu{
 
-public static void menuer(Terminal t){
+public static void putString(int r, int c,TerminalScreen t, String s) throws IOException{
+	t.setCursorPosition(new TerminalPosition(c,r));
+	for(int i = 0; i < s.length();i++){
+                t.setCharacter(r,c,new TextCharacter(s.charAt(i)));
+                r++;
+        }
+        t.setCursorPosition(null);
+}
+
+public static void menuer(TerminalScreen t) throws IOException{
 	Color c = new Color(ANSI.RED, ANSI.BLACK);
-	Print.printString(0, 0, "derp", c, t);
+	// Print.printString(0, 0, "derp", c, t);
+	putString(0,0,t,"derp");
 }
 
 }
