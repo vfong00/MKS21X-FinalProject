0315e0a4ccc16b711fb2587cfd59482c47c687d3
Vincent Fong <vfong00@stuy.edu>
2019-01-16 11:46:12 -0500

    made stairs as collectible

---
+++ src/collectible/Stairs.java
@@ -0,0 +1,10 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor.*;
+import com.googlecode.lanterna.Symbols;
+import com.googlecode.lanterna.SGR;
+
+public class Stairs extends Collectible {
+        public Stairs(int x, int y, String name, Maze map) {
+                super(x,y,new TextCharacter(Symbols.TRIANGLE_UP_POINTING_BLACK,ANSI.DEFAULT,ANSI.DEFAULT),name,map);
+        }
+}
