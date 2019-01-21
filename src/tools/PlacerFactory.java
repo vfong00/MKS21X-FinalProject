85c8b802b186c5dc87600b0e205d1cd05ae4b1e4
Moududur "Moody" Rahman <mrahman01@stuy.edu>
2019-01-20 22:23:26 -0500

    creates array of xy coordinates of all the airs

--- src/tools/PlacerFactory.java
+++ src/tools/PlacerFactory.java
@@ -1,23 +1,39 @@
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.input.*;
+import com.googlecode.lanterna.terminal.*;
+import com.googlecode.lanterna.terminal.ansi.*;
+import com.googlecode.lanterna.TextColor.*;
+import com.googlecode.lanterna.Symbols.*;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.*;
+import com.googlecode.lanterna.input.KeyType;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class PlacerFactory{
 
 Monster[] m = new Monster[20];
-int[] blankx = new int[20];
-int[] blanky = new int[20];
-public PlacerFactory(Maze m){
+ArrayList<Integer> blankx = new ArrayList<Integer>();
+ArrayList<Integer> blanky = new ArrayList<Integer>();
+public PlacerFactory(Maze m, Player p){
+	Random randgen = new Random();
 	// int x, int y, int givenXP, double hp, double damage, double defense, int accuracy, TextCharacter sprite, String name, Maze map
 
-	int c = 0;
 	for (int x = 0; x < 100; x++){
 		for (int y = 0; y < 30; y++){
-			if (m.getMaze()[x][y].getType().equals("air") && c < 21){
-				blankx[c] = x;
-				blanky[c] = y;
-				c++;
+			if (m.getMaze()[x][y].getType().equals("air")){
+				blankx.add(new Integer(x));
+				blanky.add(new Integer(y));
+
 			}
 		}
 	}
-	for (int x = 0; x < 20; x++){
-		// m[x] = new Monster()
-	}
+	Integer[] xers = blankx.toArray(new Integer[blankx.size()]);
+	Integer[] yers = blanky.toArray(new Integer[blanky.size()]);
+
+	// for (){
+	//
+	// }
 }
 }
