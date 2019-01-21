fcd2a130c967c5b9ba045db0f14bf7c73166f431
Moududur "Moody" Rahman <mrahman01@stuy.edu>
2019-01-20 19:22:51 -0500

    placer factory to handle spawn count

--- src/tools/PlacerFactory.java
+++ src/tools/PlacerFactory.java
@@ -1,8 +1,23 @@
 public class PlacerFactory{
 
-Monster[] m = new Monster[10];
-
+Monster[] m = new Monster[20];
+int[] blankx = new int[20];
+int[] blanky = new int[20];
 public PlacerFactory(Maze m){
-	
+	// int x, int y, int givenXP, double hp, double damage, double defense, int accuracy, TextCharacter sprite, String name, Maze map
+
+	int c = 0;
+	for (int x = 0; x < 100; x++){
+		for (int y = 0; y < 30; y++){
+			if (m.getMaze()[x][y].getType().equals("air") && c < 21){
+				blankx[c] = x;
+				blanky[c] = y;
+				c++;
+			}
+		}
+	}
+	for (int x = 0; x < 20; x++){
+		// m[x] = new Monster()
+	}
 }
 }
