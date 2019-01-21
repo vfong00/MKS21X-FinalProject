dbf08ce6cb1e12517013ceea0fd10e89ffa65e55
Vincent Fong <vfong00@stuy.edu>
2019-01-20 20:15:48 -0500

    wrotr javadocs for armor and collectible

--- src/collectible/Armor.java
+++ src/collectible/Armor.java
@@ -6,6 +6,16 @@ public class Armor extends Collectible {
         private Maze map;
         private String type = "collectible";
 
+        /**
+         * The constructor for the armor caollectible.
+         *
+         * @param x      The x-coordinate that the armor collectible is created at.
+         * @param y      The x-coordinate that the armor collectible is created at.
+         * @param power  The amount that this collectible increases defense by.
+         * @param sprite The char that represents the armor on the playfield.
+         * @param name   The name of the armor collectible.
+         * @param map    The map that this armor collectible belongs to.
+         */
         public Armor(int x, int y, int power, TextCharacter sprite, String name, Maze map) {
                 super(x,y,sprite,name,map);
                 this.power = power;
@@ -14,6 +24,11 @@ public class Armor extends Collectible {
                 this.map = map;
         }
 
+        /**
+         * Gives a player a buff in defense.
+         *
+         * @param p The player to which the effect is being done to.
+         */
         public void statusEffect(Player p) {
                 p.setDefense(p.getDefense() + power);
                 p.setToPrint("Picked up " + this.getName() + ". Defense increased by " + power + ".");
--- src/collectible/Collectible.java
+++ src/collectible/Collectible.java
@@ -8,6 +8,17 @@ private TextCharacter sprite;
 private String name;
 private String type = "collectible";
 
+
+/**
+ * Constructor for the collectible, the abstract class that represents an item
+ * that the player can walk to and collect for an immediate status effect.
+ *
+ * @param x      The x-coordinate that the collectible is created at.
+ * @param y      The x-coordinate that the collectible is created at.
+ * @param sprite The char that represents the collectible on the playfield.
+ * @param name   The name of the collectible.
+ * @param map    The map that this collectible belongs to.
+ */
 public Collectible(int x, int y, TextCharacter sprite, String name, Maze map) {
         this.x = x;
         this.y = y;
@@ -16,33 +27,76 @@ public Collectible(int x, int y, TextCharacter sprite, String name, Maze map) {
         map.setTile(x, y, this);
 }
 
+/**
+ * Accessor method for the x-coordinate field of this object.
+ *
+ * @return The x-coordinate of this object.
+ */
 public int getX() {
         return x;
 }
 
+/**
+ * Accessor method for the y-coordinate field of this object.
+ *
+ * @return The y-coordinate of this object.
+ */
 public int getY() {
         return y;
 }
 
+/**
+ * Accessor method for the sprite field of this object.
+ *
+ * @return A TextCharacter that is the representation this object.
+ */
 public TextCharacter getSprite() {
 	return sprite;
 }
 
+/**
+ * Accessor method for the type field of this object.
+ *
+ * @return A String that says the type of this object.
+ */
 public String getType() {
 	return type;
 }
 
+/**
+ * Accessor method for the name field of this object.
+ *
+ * @return A String that says the name of this object.
+ */
 public String getName() {
         return name;
 }
 
+/**
+ * To fulfill the Tileable interface, since the maze is of Tileables.
+ * Used for the player when determining movement.
+ *
+ * @return This collectible.
+ */
 public Collectible getCollectible() {
         return this;
 }
 
+/**
+ * To fulfill the Tileable interface, since the maze is of Tileables.
+ * This is basically useless, but is needed, otherwise the code would break.
+ *
+ * @return null
+ */
 public Entity getEntity() {
         return null;
 }
 
+/**
+ * Abstract method for extensions of this class.
+ * When the player collects this item, this effect is done to the player.
+ *
+ * @param p The player to which the effect is being done to.
+ */
 public abstract void statusEffect(Player p);
 }
